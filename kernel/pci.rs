3a01d063378354b33454638f203458adf67ef1e1
cthvlab <60858283+cthvlab@users.noreply.github.com>
2025-04-03 00:24:37 +0300

    Delete kernel/pci.rs

--- kernel/pci.rs
+++
@@ -1,5 +0,0 @@
-pub fn scan_pci_devices() {
-    println!("[PCI] Scanning PCI devices...");
-    // TODO: Реализация сканера PCI (доступ к конфигурации через порт 0xCF8/0xCFC)
-    // Это подготавливает почву для NVMe и DPDK
-}
