b9cfaadb2a5cbd0497d920fa400e5f5e9fe0dd33
cthvlab <60858283+cthvlab@users.noreply.github.com>
2025-04-03 04:10:19 +0300

    Update memory.rs

--- kernel/src/memory.rs
+++ kernel/src/memory.rs
@@ -1,3 +1,8 @@
 pub unsafe fn mmio_map<T>(phys_addr: u64) -> *mut T {
     phys_addr as *mut T
 }
+pub fn wait_cycles(cycles: u64) {
+    for _ in 0..cycles {
+        unsafe { core::arch::asm!("pause") };
+    }
+}
